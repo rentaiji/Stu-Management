5f39e9c5fefa5e16c9e05f74607a1557361fd759
Jgr <703508982@qq.com>
2026-04-03 14:57:06 +0800

    修复登录认证和静态资源访问问题，完善项目基础配置

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
         SpringApplication.run(Main.class, args);
         System.out.println("====================================");
         System.out.println("学生综合服务与教学协同平台启动成功!");
-        System.out.println("访问地址：http://localhost:8080/api");
+        System.out.println("访问地址：http://localhost:8080/index.html");
         System.out.println("====================================");
     }
 }
